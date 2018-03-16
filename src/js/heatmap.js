6bf2a93fda84ea8447bd549ade2ce536f7e6c6f7
Des Barrett <des.barrett@gmail.com>
2018-03-16 09:45:52 +0000

    xAxis workint with correct years

--- src/js/heatmap.js
+++ src/js/heatmap.js
@@ -3,7 +3,7 @@
 
 var url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json"
 //set up width, height and margins
-var margin = {top: 20, right: 20, bottom: 30, left: 30},
+var margin = {top: 20, right: 50, bottom: 30, left: 60},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -27,6 +27,15 @@ d3.json(url, (error, data) => {
   var baseTemp = data.baseTemperature;
   var dataArray = data.monthlyVariance;
 
+  var parseTime = d3.timeParse("%Y");
+  var parseMonth = d3.timeParse("%m");
+
+  // format the data
+  dataArray.forEach(function(d) {
+      d.year = parseTime(d.year);
+    //  d.month = parseMonth(d.month);
+  });
+
   // get a date range for the current year from jan 1st to jan 1st
   //{"year": 2014, "month": 11,"variance": 0.632 },
   var start = d3.min(dataArray,function(d) { return +d.year; }),
@@ -37,6 +46,11 @@ d3.json(url, (error, data) => {
     .range([0, width])
     .domain([start, end]);
 
+
+  // var y = d3.scaleTime()
+  //     .domain([new Date("2018-01-01"), new Date("2018-31-12")])
+  //     .range([height, 0]);
+  //
   var y = d3.scaleLinear()
   .range([height, 0])
   .domain([0, 12]);
@@ -44,14 +58,17 @@ d3.json(url, (error, data) => {
   //x and y axis
   var xAxis = d3.axisBottom(x);
   var yAxis = d3.axisLeft(y);
+                //.tickFormat(d3.timeFormat("%B"));
 
   //grid width and height
   var gridWidth = width / 252;
   var gridHeight = height / 12;
 
+  var formatMonth = d3.timeFormat("-%m");
 
-
-
+  var now = new Date(),
+      ystart = d3.timeYear.floor(now),
+      yend = d3.timeYear.ceil(now);
 
 // data now has objects as below
 //{day1: Mon Jan 01 2018 00:00:00 GMT+0000 (GMT Standard Time), day2: Wed Jan 03 2018 00:00:00 GMT+0000 (GMT Standard Time), lat1: -90, lat2: -88, altitude: 24.007023982873225}
@@ -72,67 +89,3 @@ svg.append("g")
   .call(yAxis);
 
 });
-
-
-
-
-
-  // var heatmapChart = function(tsvFile) {
-  //       d3.tsv(tsvFile,
-  //       function(d) {
-  //         return {
-  //           day: +d.day,
-  //           hour: +d.hour,
-  //           value: +d.value
-  //         };
-  //       },
-  //       function(error, data) {
-  //         var colorScale = d3.scale.quantile()
-  //             .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
-  //             .range(colors);
-  //
-  //         var cards = svg.selectAll(".hour")
-  //             .data(data, function(d) {return d.day+':'+d.hour;});
-  //
-  //         cards.append("title");
-  //
-  //         cards.enter().append("rect")
-  //             .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-  //             .attr("y", function(d) { return (d.day - 1) * gridSize; })
-  //             .attr("rx", 4)
-  //             .attr("ry", 4)
-  //             .attr("class", "hour bordered")
-  //             .attr("width", gridSize)
-  //             .attr("height", gridSize)
-  //             .style("fill", colors[0]);
-  //
-  //         cards.transition().duration(1000)
-  //             .style("fill", function(d) { return colorScale(d.value); });
-  //
-  //         cards.select("title").text(function(d) { return d.value; });
-  //
-  //         cards.exit().remove();
-  //
-  //         var legend = svg.selectAll(".legend")
-  //             .data([0].concat(colorScale.quantiles()), function(d) { return d; });
-  //
-  //         legend.enter().append("g")
-  //             .attr("class", "legend");
-  //
-  //         legend.append("rect")
-  //           .attr("x", function(d, i) { return legendElementWidth * i; })
-  //           .attr("y", height)
-  //           .attr("width", legendElementWidth)
-  //           .attr("height", gridSize / 2)
-  //           .style("fill", function(d, i) { return colors[i]; });
-  //
-  //         legend.append("text")
-  //           .attr("class", "mono")
-  //           .text(function(d) { return "≥ " + Math.round(d); })
-  //           .attr("x", function(d, i) { return legendElementWidth * i; })
-  //           .attr("y", height + gridSize);
-  //
-  //         legend.exit().remove();
-  //
-  //       });
-  //     };
