f6fa979559637b627d77991ddc413cbbe313f8d7
Des Barrett <des.barrett@gmail.com>
2018-03-17 20:19:26 +0000

    styled and working

--- src/js/heatmap.js
+++ src/js/heatmap.js
@@ -1,31 +1,47 @@
 // Fcc Visualize Data with a Heat Map using D3.js
 
-
+// url for json data
 var url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json"
-//set up width, height and margins
-var margin = {top: 20, right: 50, bottom: 80, left: 80},
+
+// standard d3 margins set up width and height
+var margin = {top: 70, right: 50, bottom: 80, left: 80},
     width = 960 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+    height = 600 - margin.top - margin.bottom;
 
 var months =  ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
 //color scale
 var color = d3.scaleLinear()
 .domain([12, 9, 6, 3, 0])
-.range(['#D7191C', '#FDAE61','#EDBD71', '#ABD9E9', '#2C7BB6']);
+.range(['#D7191C', '#FDAE61','#f2d7ab', '#71c8e7', '#2C7BB6']);
 
 //tooltip div
 var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-// add svg to body
+// add svg and group to body
 var svg = d3.select('#container').append('svg')
-.attr('width', width + margin.left + margin .right)
-.attr('height', height + margin.top + margin.bottom)
-.append('g')
-.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+            .attr("class", "shadow")
+            .attr("id", "chart")
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+// text for title
+var title = svg.append("text")
+              .attr("id", "title")
+              .attr("text-anchor", "middle")
+              .text("Monthly Global Land Surface Temperatures: 1753-2015")
+              .attr('transform', `translate(${width/2},${-35})`);
+
+// description
+var description = svg.append("text")
+                  .attr("id", "description")
+                  .attr("text-anchor", "middle")
+                  .text("Temperatures are reported as anomalies relative to the Jan 1951-Dec 1980 average.")
+                  .attr('transform', `translate(${width/2},${-18})`);
 //get data
 d3.json(url, (error, data) => {
   if(error) console.log("Problem loading json data: " + error);
@@ -43,30 +59,30 @@ d3.json(url, (error, data) => {
 
   });
 
-  // get a date range for the current year from jan 1st to jan 1st
+  // get a date range from our data
   var start = d3.min(dataArray,function(d) { return +d.year; }),
       end = d3.max(dataArray,function(d) { return +d.year; });
 
   //scales
   var x = d3.scaleTime()
     .range([0, width])
-    .domain([start, end]);
+    .domain([start, end]); //add date range to domain
 
   var y = d3.scaleLinear()
   .range([height, 0])
   .domain([13, 1]);
 
-  //x and y axis
+  // add x-axis
   var xAxis = d3.axisBottom(x);
 
-
+//  add y-axis as text
   svg.append('g')
   .selectAll('text')
   .data(months)
   .enter().append('text')
   .attr('class','months')
-  .attr('x', function(d) { return -10; }) //(d) => `${-10}`)
-  .attr('y', function(d, i) { return 20 + (i * 38); }) //.attr('y', (d, i) => `${20 + (i * 33.4)}`)
+  .attr('x', function(d) { return -10; })
+  .attr('y', function(d, i) { return 20 + (i * 38); })
   .attr('text-anchor','end')
   .text((d) => `${d}`);
 
@@ -74,13 +90,7 @@ d3.json(url, (error, data) => {
   var gridWidth = width / 252;
   var gridHeight = height / 12;
 
-  var formatMonth = d3.timeFormat("%B");
-  console.log(formatMonth(4));
-  var now = new Date(),
-      ystart = d3.timeYear.floor(now),
-      yend = d3.timeYear.ceil(now);
-
-
+// add the svg rects and color them based on the temp data
 svg.selectAll('.cell')
   .data(dataArray)
 .enter().append('rect')
@@ -89,6 +99,7 @@ svg.selectAll('.cell')
   .attr('width', gridWidth)
   .attr('height', gridHeight)
   .attr('fill', function (d) { return color(baseTemp + d.variance); })
+  //add the tooltip which moves and transitons a div
   .on("mouseover", function(d) {
       div.transition()
         .duration(200)
@@ -104,11 +115,13 @@ svg.selectAll('.cell')
         .style("opacity", 0);
       });
 
+// positon the x-axis
 svg.append("g")
   .attr("class", "x axis")
   .attr("transform", "translate(0," + (height ) + ")")
   .call(xAxis);
 
+//linearGradient for color/temp comparision
 var linearGradient = svg.append('linearGradient')
   .attr('id', 'linear-gradient');
 
@@ -123,21 +136,19 @@ svg.append('rect')
   .attr('height', 20)
   .style('fill', 'url(#linear-gradient)')
   .attr('transform', 'translate(300,475)');
+
+// convert to strings from colordomain
 var colorDom = color.domain().map(String);
-  svg.append('g')
-    .selectAll('text')
-    .data(colorDom)
-    .enter().append('text')
-    .attr('class','temperatures')
-    .attr('x', (d) => `${590 - (Math.ceil(300 / 13) * d)}`)
-    .attr('y', '515')
-    .text((d) => d);
 
 
-// svg.append("g")
-//   .attr("class", "y axis")
-//   .call(yAxis);
-
-
+// add the temps under the color gradient and space
+svg.append('g')
+  .selectAll('text')
+  .data(colorDom)
+  .enter().append('text')
+  .attr('class','temperatures')
+  .attr('x', (d) => `${590 - (Math.ceil(300 / 13) * d)}`)
+  .attr('y', '515')
+  .text((d) => d);
 
 });
